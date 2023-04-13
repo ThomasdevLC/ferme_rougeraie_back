18cae1bd9f060c07cfe60ff1f42b04460321d6e7
ThomasdevLC <thomas.lecam@gmail.com>
2023-04-13 11:46:16 +0200

    model

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  likers: {
+    type: [String],
+  },
+});
+
+module.exports = mongoose.model("post", postSchema);
